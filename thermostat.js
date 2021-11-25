1707a9571bff458d76b87ef5af6ddf3cef341ac8
88Andybird <anbird88@gmail.com>
2021-11-25 14:47:32 +0000

    Added powersaving mode and energy usage method. All working and tests passing. All criteria tested and satisfied

--- thermostat.js
+++ thermostat.js
@@ -2,23 +2,98 @@ class Thermostat {
   constructor(){
       this.temperature = 20;
       this.minTemperature = 10;
+      this.powerSavingMode = true;
   }
   getTemperature() {
-      if (this.temperature < this.minTemperature) {
-          return this.minTemperature; 
-      } else {
           return this.temperature
-      }
+      };
      
-  };
+  
 
   up() {
-      this.temperature++ 
-  };
+      if (this.powerSavingMode === true) {
+          if (this.temperature === 25) {
+              return 'max temp reached'
+          } else {
+              this.temperature++
+          }
+          } else {
+              if (this.temperature === 32) {
+                  return 'max temp reached'
+              } else {
+                  this.temperature++
+              }
+          }
+      }
+    
+    
+      
+      
+  
+  
   down() {
-    this.temperature-- 
+    if (this.temperature > 10) {
+    this.temperature--
+    } else {
+        this.temperature = 10;
+    }
+  };
+
+  setPowerSavingMode(condition) {
+      if (condition === true) {
+          this.powerSavingMode = true;
+      } else {
+          this.powerSavingMode = false;
+      }
+      };
+
+    reset() {
+        this.temperature = 20;
+    }
+
+    energyUsage() {
+        if (this.temperature < 18) {
+            return 'low energy usage';
+        } else if (this.temperature <= 25) {
+             return 'medium energy usage';
+        } else if (this.temperature > 25) {
+             return 'High Energy Usage';
+        }
+    };
+
+    
+  
+
 };
+
+const thermostat = new Thermostat();
+
+console.log(thermostat.getTemperature()); // should return 20
+
+thermostat.up();
+thermostat.up();
+console.log(thermostat.getTemperature()); // should now return 22
+
+thermostat.down();
+console.log(thermostat.getTemperature()); // should now return 21
+
+thermostat.setPowerSavingMode(true); // PSM is now on, max temperature is 25
+
+for (let i = 0 ; i < 10 ; i++) {
+  thermostat.up();
 }
 
+console.log(thermostat.getTemperature()); // should be 25 (max reached)
+
+thermostat.setPowerSavingMode(false); // PSM is now off, max temperature is no more 25
+
+thermostat.up();
+console.log(thermostat.getTemperature()); // should now return 26
+
+thermostat.reset();
+console.log(thermostat.getTemperature()); // should be back to 20
 
 module.exports = Thermostat
+
+
+  
--- thermostat.test.js
+++ thermostat.test.js
@@ -29,5 +29,46 @@ describe("thermostat",() => {
 
     })
 
+    it('can turn low power mode off', () => {
+        const thermostat = new Thermostat;
+        thermostat.setPowerSavingMode(false)
+        expect(thermostat.powerSavingMode).toBe(false) 
+    })
+
+    it('cannot be set higher than 25 if lps mode is on', () => {
+        const thermostat = new Thermostat;
+        for(let i=0 ; i<15; i++){
+            thermostat.up()
+        }
+        expect(thermostat.getTemperature()).toBe(25)
+
+    })
+
+    it('cannot be set higher than 32 when lps mode is off',() => {
+        const thermostat = new Thermostat;
+        thermostat.setPowerSavingMode(false)
+        for(let i=0 ; i<15; i++){
+            thermostat.up()
+        }
+        expect(thermostat.getTemperature()).toBe(32)
+    })
+
+    it('can reset the temp to 20',() => {
+        const thermostat = new Thermostat;
+        thermostat.down()
+        thermostat.down()
+        expect(thermostat.getTemperature()).toBe(18)
+        thermostat.reset()
+        expect(thermostat.getTemperature()).toBe(20)
+
+    })
+
+    it('can return energy usage depending on temp',() => {
+        const thermostat = new Thermostat;
+        expect(thermostat.energyUsage()).toBe('medium energy usage')
+    })
+
+  
+
 });
 
